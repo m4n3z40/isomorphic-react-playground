403b7d74fc6b44aebc3a71cc1401a948533991b0
Allan Baptista <allan.marques@ymail.com>
2014-12-13 16:07:06 -0200

    Tasks store documented.

--- stores/tasks.js
+++ stores/tasks.js
@@ -4,8 +4,18 @@ var _ = require('lodash'),
     savedTask;
 
 module.exports = Store.extend({
+    /**
+     * Identifier of the store
+     *
+     * @type {string}
+     */
     name: 'TasksStore',
 
+    /**
+     * Returns the default handlers for actions that the store listens to
+     *
+     * @return {Object}
+     */
     getDefaultHandlers: function() {
         var handlers = {};
 
@@ -21,10 +31,21 @@ module.exports = Store.extend({
         return handlers;
     },
 
+    /**
+     * Initializes the store
+     *
+     * @returns {void}
+     */
     initialize: function() {
         this.tasks = [];
     },
 
+    /**
+     * Returns a new task object
+     *
+     * @param {string} task
+     * @return {Object}
+     */
     createTask: function(task) {
         return {
             id: 'tsk_' + (new Date()).valueOf(),
@@ -34,38 +55,80 @@ module.exports = Store.extend({
         };
     },
 
+    /**
+     * Returns all tasks present now in the store
+     *
+     * @return {Array}
+     */
     getAll: function() {
         return this.tasks;
     },
 
+    /**
+     * Returns the current state of the store for being restored later
+     *
+     * @return {Object}
+     */
     saveState: function() {
-        return {
-            tasks: this.tasks
-        };
+        return {tasks: this.tasks};
     },
 
+    /**
+     * Restores the saved state of the store
+     *
+     * @param {string} state
+     * @return {void}
+     */
     restoreState: function(state) {
-        this.tasks = state.tasks;
+        this.tasks = state.tasks || [];
     },
 
+    /**
+     * Handler for when the retrieve_success action is executed
+     *
+     * @param {Array} tasks
+     * @return {void}
+     * @protected
+     */
     _onRetrieveSuccess: function(tasks) {
         this.tasks = tasks;
 
         this.emitChanges();
     },
 
+    /**
+     * Handler for when the create_start action is executed
+     *
+     * @param {Object} task
+     * @return {void}
+     * @protected
+     */
     _onCreateStart: function(task) {
         this.tasks.push(task);
 
         this.emitChanges();
     },
 
+    /**
+     * Handler for when the create_error action is executed
+     *
+     * @param {Error} error
+     * @return {void}
+     * @protected
+     */
     _onCreateError: function(error) {
         this.tasks.pop();
 
         this.emitChanges();
     },
 
+    /**
+     * Handler for when the update_start action is executed
+     *
+     * @param {Object} task
+     * @return {void}
+     * @protected
+     */
     _onUpdateStart: function(task) {
         var oldTask = _.first(this.tasks, {id: task.id});
 
@@ -79,6 +142,13 @@ module.exports = Store.extend({
         }
     },
 
+    /**
+     * Handler for when the update_error action is executed
+     *
+     * @param {Error} error
+     * @return {void}
+     * @protected
+     */
     _onUpdateError: function(error) {
         var newTask = _.first(this.tasks, {id: savedTask.id});
 
@@ -92,6 +162,13 @@ module.exports = Store.extend({
         }
     },
 
+    /**
+     * Handler for when the destroy_start action is executed
+     *
+     * @param {Object} task
+     * @return {void}
+     * @protected
+     */
     _onDestroyStart: function(task) {
         _.remove(this.tasks, {id: task.id});
 
@@ -100,7 +177,13 @@ module.exports = Store.extend({
         this.emitChanges();
     },
 
-    _onDestroyError: function(error) {
+    /**
+     * Handler for when the destroy_error action is executed
+     *
+     * @return {void}
+     * @protected
+     */
+    _onDestroyError: function() {
         this.tasks.push(savedTask);
 
         savedTask = null;
@@ -108,6 +191,12 @@ module.exports = Store.extend({
         this.emitChanges();
     },
 
+    /**
+     * Handler for when the filter action is executed
+     *
+     * @param {Object} filter
+     * @protected
+     */
     _onFilter: function(filter) {
         var tasks = this.tasks;
 
